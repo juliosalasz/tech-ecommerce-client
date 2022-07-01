78971d629e8577ea7e4ead7b9e7b46c2b0edb7e4
Julio Salas <jscomic@gmail.com>
2022-07-01 00:41:21 -0500

    redux persist, local env logger and redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,37 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import logger from "redux-logger";
 
 import { rootReducer } from "./rootReducer";
 
-const middlewares = [logger];
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middlewares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
+
+const composeEnhancers =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSIO_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancers(applyMiddleware(...middlewares));
 
 //root reducer
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
